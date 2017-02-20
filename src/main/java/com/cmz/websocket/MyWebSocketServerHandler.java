f867ecafe5633ee4d4eea519764728167d5f8cab
chenmingzhou <sd@dfh.com>
2017-02-20 11:38:14 +0800

    websocket

--- src/main/java/com/cmz/websocket/MyWebSocketServerHandler.java
+++ src/main/java/com/cmz/websocket/MyWebSocketServerHandler.java
@@ -3,14 +3,13 @@ package com.cmz.websocket;
 import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.channel.SimpleChannelInboundHandler;
-import io.netty.handler.codec.http.DefaultFullHttpRequest;
 import io.netty.handler.codec.http.DefaultFullHttpResponse;
 import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.HttpResponseStatus;
@@ -88,7 +87,7 @@ public class MyWebSocketServerHandler extends ChannelInboundHandlerAdapter {
 	}
 
 	private void handleHttpRequest(ChannelHandlerContext ctx, FullHttpRequest req) {
-		if (!req.getDecoderResult().isSuccess() || (!"websocket".equals(req.headers().get("Upgrade")))) {
+		if (!req.decoderResult().isSuccess() || (!"websocket".equals(req.headers().get("Upgrade")))) {
 			sendHttpResponse(ctx, req,
 					new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.BAD_REQUEST));
 			return;
@@ -97,7 +96,7 @@ public class MyWebSocketServerHandler extends ChannelInboundHandlerAdapter {
 				"ws://localhost:7397/websocket", null, false);
 		handshaker = wsFactory.newHandshaker(req);
 		if (handshaker == null) {
-			WebSocketServerHandshakerFactory.sendUnsupportedWebSocketVersionResponse(ctx.channel());
+			WebSocketServerHandshakerFactory.sendUnsupportedVersionResponse(ctx.channel());
 		} else {
 			handshaker.handshake(ctx.channel(), req);
 		}
@@ -105,14 +104,14 @@ public class MyWebSocketServerHandler extends ChannelInboundHandlerAdapter {
 
 	private static void sendHttpResponse(ChannelHandlerContext ctx, FullHttpRequest req, DefaultFullHttpResponse res) {
 		// 返回应答给客户端
-		if (res.getStatus().code() != 200) {
-			ByteBuf buf = Unpooled.copiedBuffer(res.getStatus().toString(), CharsetUtil.UTF_8);
+		if (res.status().code() != 200) {
+			ByteBuf buf = Unpooled.copiedBuffer(res.status().toString(), CharsetUtil.UTF_8);
 			res.content().writeBytes(buf);
 			buf.release();
 		}
 		// 如果是非Keep-Alive，关闭连接
 		ChannelFuture f = ctx.channel().writeAndFlush(res);
-		if (!isKeepAlive(req) || res.getStatus().code() != 200) {
+		if (!isKeepAlive(req) || res.status().code() != 200) {
 			f.addListener(ChannelFutureListener.CLOSE);
 		}
 	}
